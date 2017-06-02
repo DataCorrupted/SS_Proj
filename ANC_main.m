f06bf6ead40b238cf368860d841f4eebf0a098f2
PeterRong <rongyy@shanghaitech.edu.cn>
2017-06-02 10:34:46 +0800

    ANC_main.m: now it's working.
    with the help of dsp.LMSFilter, now it's working, I'm going to add a
    double channel.
    
    Signed-off-by: PeterRong <rongyy@shanghaitech.edu.cn>

--- ANC_main.m
+++ ANC_main.m
@@ -1,6 +1,4 @@
 clear
-% Noise = audioDeviceReader;
-% setup(Nosie);% Initialize audio device
 SPF = 1024;                                 % SamplesPerFrame
 Noise = dsp.AudioFileReader( ...
     'Filename','./WhiteNoise.wav', ...
@@ -10,49 +8,27 @@ Signal = dsp.AudioFileReader(...
     'SamplesPerFrame', SPF);
 devWriter = audioDeviceWriter;
 
-L = 4;
-e = zeros(1, L);
-x = zeros(1, L);
-y = zeros(1, L);
-w = zeros(1, L);
+NLMS = dsp.LMSFilter('Length', 16, ...
+   'Method', 'Normalized LMS',...
+   'AdaptInputPort', true, ...
+   'StepSizeSource', 'Input port', ...
+   'WeightsOutputPort', false);
+FIR = dsp.FIRFilter('Numerator', fir1(8,[.25, .75]));
 
-P = 1;
-c = 1;
-alpha = 0.99;
-
-mu = 0.003;
-mimc = 0;
+a = 1;      % adaptation control
+mu = 0.05;  % step size
 
 tic;
-while toc < 20
-  %[error, nOverrun] = record(devReader);
-  % take noise
+while toc < 30
   signal_frame = step(Signal);
   noise_frame = step(Noise);
   
-  cancel_frame = zeros(SPF, 1);
-  cancel_frame(1, 1) = mimc;
-  
-  for samp_cnt = 1: SPF
-      samp = noise_frame(samp_cnt) - mimc;  % Take the psudo sample
-      e = [samp e(1: L-1)];                 % add it to history
-      x = y + e;                            % calculate the estimate noise
-      mimc = x * w';                        % mimc the next noise
-      y = [mimc y(1: L-1)];                 % add it to mimc history
-      if samp_cnt ~= SPF
-          cancel_frame(samp_cnt + 1) = mimc;% add it to play frame
-      end
-      P = alpha * (x * x') + (1-alpha) * P; % Update P & w
-      w = w - mu * samp * x / (P + c);
-  end
-  plot(noise_frame - cancel_frame);
-  pause
-  output = signal_frame;
-  % play(devWriter, output);
-  % step(fileWriter, output);
+  mixture_frame = FIR(noise_frame) + signal_frame; % Noise + Signal
+  [ ~ , result_frame] = NLMS(noise_frame, mixture_frame, mu, a);
+  output = result_frame;
+  play(devWriter, output);
 end
 
-    
 release(Noise);
 release(Signal);
 disp('Noise Canceler exited.');
